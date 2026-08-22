de22ede9e287ef39971d86ee3f538b998e430fa9
Krzysiek-Mistrz <krzyskuar@gmail.com>
2026-08-23 00:14:25 +0200

    chore: finish OGContinue rebrand (status bar, command palette, tutorial, icons), well at least I hope...

--- extensions/vscode/continue_tutorial.py
+++ extensions/vscode/continue_tutorial.py
@@ -1,8 +1,4 @@
-"""                    _________               _____ _____
-                       __  ____/______ _______ __  /____(_)_______ ____  _______
-                       _  /     _  __ \__  __ \_  __/__  / __  __ \_  / / /_  _ \
-                       / /___   / /_/ /_  / / // /_  _  /  _  / / // /_/ / /  __/
-                       \____/   \____/ /_/ /_/ \__/  /_/   /_/ /_/ \__,_/  \___/
+"""                                  OGContinue
 
                                  Chat, Edit, and Autocomplete tutorial
 """
@@ -12,7 +8,7 @@
 
 ## Highlight the code below
 ## Press [Cmd/Ctrl + L] to add to Chat
-## Try asking Continue "what sorting algorithm is this?"
+## Try asking OGContinue "what sorting algorithm is this?"
 def sorting_algorithm(x):
     for i in range(len(x)):
         for j in range(len(x) - 1):
@@ -25,7 +21,7 @@ def sorting_algorithm(x):
 
 ## Highlight the code below
 ## Press [Cmd/Ctrl + I] to Edit
-## Try asking Continue to "make this more readable"
+## Try asking OGContinue to "make this more readable"
 def sorting_algorithm(x):
     for i in range(len(x)):
         for j in range(len(x) - 1):
@@ -41,4 +37,4 @@ def sorting_algorithm(x):
 # Basic assertion for sorting_algorithm...
 
 
-# ——————————————————      Learn more at https://docs.continue.dev/getting-started/overview      ——————————————————— #
+# ——————————————————      Learn more: github.com/Krzysiek-Mistrz/OGContinue      ——————————————————— #
